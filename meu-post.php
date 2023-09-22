6fc0aa41198558a8063c035e56897f29f38bc87f
BDBento <bdegan2016@gmail.com>
2023-09-21 23:25:50 -0400

    2

--- meu-post.php
+++ meu-post.php
@@ -6,6 +6,7 @@ Author: bbento
 */
  
 add_action( 'init', 'custom_post_socio' );
+
  
 function custom_post_socio() {
  
@@ -35,7 +36,40 @@ function custom_post_socio() {
     'has_archive'       => true,
     'query_var'         => 'socio'
   );
- 
+
+
+  
   register_post_type( 'socio', $args);
 }
+
+add_action( 'init', 'custom_post_colaborador' );
+
+function custom_post_colaborador() {
  
+    $labels = array(
+      'name'               => __( 'Colaborador' ),
+      'singular_name'      => __( 'Colaborador' ),
+      'add_new'            => __( 'Add novo Colaborador' ),
+      'add_new_item'       => __( 'Add novo Colaborador' ),
+      'edit_item'          => __( 'Editar Colaborador' ),
+      'new_item'           => __( 'novo Colaborador' ),
+      'all_items'          => __( 'Todos Colaborador' ),
+      'view_item'          => __( 'Ver Colaborador' ),
+      'search_items'       => __( 'Procurar Colaborador' ),
+    );
+   
+    $args = array(
+      'labels'            => $labels,
+      'description'       => 'Mantém nossos colaboradores e dados específicos dos colaborador',
+      'public'            => true,
+      'menu_position'     => 5,
+      'supports'          => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+      'has_archive'       => true,
+      'show_in_admin_bar' => true,
+      'show_in_nav_menus' => true,
+      'has_archive'       => true,
+      'query_var'         => 'colaborador'
+    );
+ 
+  register_post_type( 'colaborador', $args);
+}
